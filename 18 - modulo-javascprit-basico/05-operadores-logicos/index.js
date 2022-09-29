3afda70d5ddcb651d2f5fbe6c366c37cd8b1e682
luispaulobrito <luiss_paullo@hotmail.com>
2022-09-29 15:08:39 -0300

    operadores logicos

---
+++ 18 - modulo-javascprit-basico/05-operadores-logicos/index.js
@@ -0,0 +1,12 @@
+/*
+    == compara se o conteúdo é igual
+    === compara se o conteúdo e o tipo da variável são iguais
+    != não igual
+    !== não idêntico
+*/
+
+const a = 3
+const b = "3"
+
+console.log(a==b)
+console.log(a===b)
