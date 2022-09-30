94a3f1c46dd0c41592b77fc34886546e78ca6f3b
luispaulobrito <luiss_paullo@hotmail.com>
2022-09-30 15:13:21 -0300

    lacos de repeticao

--- 18 - modulo-javascprit-basico/05-operadores-logicos/index.js
+++ 18 - modulo-javascprit-basico/05-operadores-logicos/index.js
@@ -3,10 +3,16 @@
     === compara se o conteúdo e o tipo da variável são iguais
     != não igual
     !== não idêntico
+    && = e
+    || = ou
+    ! = negação
 */
 
 const a = 3
-const b = "3"
+const b = 3
 
 console.log(a==b)
 console.log(a===b)
+console.log(a===b && a>b)
+console.log(a===b || a>b)
+console.log(!(a===b))
---
+++ 18 - modulo-javascprit-basico/06-lacos/index.js
@@ -0,0 +1,23 @@
+// FOR
+
+for (let i = 0; i<5; i++) {
+    console.log(i);
+}
+
+// // WHILE
+
+let a = 10
+let b = 25
+
+while (a<b) {
+        console.log('incrementando a variável a '+ a)
+    a++
+}
+
+//DO WHILE
+
+let c = 10
+do {
+    console.log(c)
+    c++
+} while (a<=18);
