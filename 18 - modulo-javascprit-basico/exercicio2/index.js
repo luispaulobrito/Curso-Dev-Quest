e41a3ca83e8d7c384e8ccefd74c3fb0fca3a337c
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-01 17:38:22 -0300

    exercicio2

---
+++ 18 - modulo-javascprit-basico/exercicio2/index.js
@@ -0,0 +1,11 @@
+let boletoPago = false
+
+function verificarBoletoPago(boletoPago) {
+    if (boletoPago) {
+        alert('O boleto está pago')
+    }else{
+        alert('O boleto NÃO está pago')
+    }
+}
+
+verificarBoletoPago(boletoPago)
