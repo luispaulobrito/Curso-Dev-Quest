9da45cb95b3b0adc2dc0a8c04a5fa0f29226af78
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-01 16:56:00 -0300

    condicionais

---
+++ 18 - modulo-javascprit-basico/07-condicionais/index.js
@@ -0,0 +1,26 @@
+/*
+let nomeFilme = 'Vingadores'
+
+if (nomeFilme == "Vingadores") {
+    console.log('True')
+}else{
+    console.log('False')
+}
+
+// OPERADOR TERNÁRIO
+
+1+1==2 ? console.log('verdadeiro') : console.log('falso')
+*/
+
+// SWITCH
+
+let nomeFilme = "Homem Aranha"
+    switch (nomeFilme) {
+        case "MIB":
+            console.log("MIB")
+            break;
+    
+        default:
+            console.log("Outro filme")
+            break;
+    }
