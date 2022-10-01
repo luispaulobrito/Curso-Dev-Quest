6f47d25b7503a6f23008ba201d5b525e363e376a
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-01 17:28:46 -0300

    exercicio1

---
+++ 18 - modulo-javascprit-basico/exercicio1/index.js
@@ -0,0 +1,13 @@
+let nome = prompt('Digite seu nome')
+alert('Seja bem vindo, '+ nome +'!')
+
+// Usando uma função seria assim:
+/*
+let nome = prompt('Digite seu nome')
+
+function bemVindo(nome) {
+    alert('Seja bem vindo, '+ nome +'!')
+}
+
+bemVindo(nome)
+*/
