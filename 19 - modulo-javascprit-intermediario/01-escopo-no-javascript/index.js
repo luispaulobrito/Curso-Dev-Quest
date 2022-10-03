5ecdc366f1dd39573a0fe2bbf82a91d2236e9567
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-03 18:04:32 -0300

    escopo js

---
+++ 19 - modulo-javascprit-intermediario/01-escopo-no-javascript/index.js
@@ -0,0 +1,19 @@
+// if (true) {
+//     const mensagem = 'Olá'
+//     console.log(mensagem)
+// }
+
+// if (true) {
+//     var mensagem = 'Olá'   
+// }
+// console.log(mensagem)
+
+// function falar() {
+//     var mensagem = "Olá mundo"
+//     console.log(mensagem)
+// }
+
+// falar()
+// console.log(mensagem)
+
+console.log(url)
