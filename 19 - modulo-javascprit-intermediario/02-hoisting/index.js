c1a2375a7ccbadb54772391431f09f5fd3a56e7a
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-04 15:30:00 -0300

    functions

---
+++ 19 - modulo-javascprit-intermediario/02-hoisting/index.js
@@ -0,0 +1,20 @@
+/*
+var pais //declaracao
+pais = 'Brasil' //atribuição
+
+var pais = 'Brasil' //embora pareça ter acontecido na mesma linha, são duas instruções separadar igual acima
+*/
+
+// console.log(pais)
+// var pais = 'Brasil'
+
+var pais
+console.log(pais)
+pais = 'Brasil'
+
+// console.log(pais)
+// let pais = 'Brasil'
+
+// let pais
+// console.log(pais)
+// pais = 'Brasil'
---
+++ 19 - modulo-javascprit-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,29 @@
+// // Função Anônima
+
+// let resultadoDaSoma = function (numero1, numero2) {
+//     return numero1+numero2
+// }
+
+// console.log(resultadoDaSoma(12,5))
+
+// Arrow function
+
+// let resultadoDaSoma = (numero1, numero2) => {
+//     return numero1+numero2
+// }
+
+// console.log(resultadoDaSoma(12,5))
+
+// const incentivarQuester = nomeQuester => {
+//     console.log('Parabéns, '+nomeQuester+'! Você chegou ao módulo JS Intermediário')
+// }
+
+// incentivarQuester('Luis')
+
+// let resultadoDaSoma = (numero1, numero2) => numero1+numero2
+
+// console.log(resultadoDaSoma(12,5))
+
+const incentivarQuest = nomeQuester => console.log(nomeQuester)
+
+incentivarQuest('Paulo')
