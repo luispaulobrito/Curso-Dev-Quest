97bf41a0cd1592ab98b14096765780830981523b
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-06 15:57:19 -0300

    slice aplice

--- 19 - modulo-javascprit-intermediario/06-arrays/index.js
+++ 19 - modulo-javascprit-intermediario/06-arrays/index.js
@@ -1,8 +1,28 @@
 let filmes = ['Vingadores', 'Mulher Maravilha', 'Venon','Feita na América']
 
+console.log(filmes.indexOf('Venon'))
+
+
+
+
+/*
 for (let index = 0; index < filmes.length; index++) {
     const element = filmes[index];
     console.log(element)
 }
+*/
+
+/*
+filmes.forEach(item => {
+    console.log(item)
+});
+*/
+
+/*
+filmes.forEach(function(item) {
+    console.log(item)
+});
+*/
+
 
 // let avaliacoes = [1, 4.6, 5]
---
+++ 19 - modulo-javascprit-intermediario/08-slice-splice/index.js
@@ -0,0 +1,15 @@
+let frutas = ['Banana', 'Maçã', 'Uva', 'Laranja', 'Pera']
+
+let removerEAdicionar = frutas.splice(1, 2, 'Goiaba', 'Abacaxi', 'Melão')
+console.log(removerEAdicionar)
+console.log(frutas)
+/*
+let frutasExtraidas = frutas.splice(1, 3)
+console.log(frutasExtraidas)
+console.log(frutas)
+*/
+
+/*
+let frutasExtraidas = frutas.slice(1, 3)
+console.log(frutasExtraidas)
+*/
