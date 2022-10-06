c5adf7c90f4e421e26764d9f36c17173c573a53b
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-06 14:36:10 -0300

    arrays

---
+++ 19 - modulo-javascprit-intermediario/06-arrays/index.js
@@ -0,0 +1,8 @@
+let filmes = ['Vingadores', 'Mulher Maravilha', 'Venon','Feita na América']
+
+for (let index = 0; index < filmes.length; index++) {
+    const element = filmes[index];
+    console.log(element)
+}
+
+// let avaliacoes = [1, 4.6, 5]
