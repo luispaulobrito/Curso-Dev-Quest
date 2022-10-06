c59c4ec0d6c909502b2f248704bed6e0e406d900
luispaulobrito <luiss_paullo@hotmail.com>
2022-10-06 14:57:13 -0300

    array multimensional

---
+++ 19 - modulo-javascprit-intermediario/07-array-multidimensional/index.js
@@ -0,0 +1,17 @@
+let filmes = [  
+                ['Mulher Maravilha', 5.0, true], 
+                ['Venon', 3.2, false],
+                ['Vingadores', 4.5, true]
+            ]
+
+console.table(filmes)            
+
+//percorrendo todo o array com for
+/*            
+for (let i = 0; i < filmes.length; i++) {
+    for (let j = 0; j < filmes.length; j++) {
+        const element = filmes[i][j];
+        console.log(element)
+    }
+}
+*/
