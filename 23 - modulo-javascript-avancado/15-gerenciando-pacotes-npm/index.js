4d8b0a46effbf847b9a0f4345b03c4586fc4ac0f
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-03 14:17:51 -0300

    modulos

---
+++ 23 - modulo-javascript-avancado/15-gerenciando-pacotes-npm/index.js
@@ -0,0 +1,5 @@
+const kind_of = require("kind-of")
+const testeDeBoolean = true
+const testeDeTexto = 'texto'
+console.log(kind_of(testeDeBoolean))
+console.log(kind_of(testeDeTexto))
---
+++ 23 - modulo-javascript-avancado/16-modulos-javascript/index.js
@@ -0,0 +1,4 @@
+import { multiplicar, somar } from "./operacoes-matematicas.js";
+
+console.log(somar(5,3))
+console.log(multiplicar(5,3))
