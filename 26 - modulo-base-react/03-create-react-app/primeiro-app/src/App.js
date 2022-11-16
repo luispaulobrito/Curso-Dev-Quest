b35cdc04cd4893655e188f2d1a3d613a1930b946
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-16 11:36:48 -0300

    botao clique aqui criado

--- 26 - modulo-base-react/03-create-react-app/primeiro-app/src/App.js
+++ 26 - modulo-base-react/03-create-react-app/primeiro-app/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "./components/button/button.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, my name is Luis!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Button/>
+    <Button/>
+    <Button/>
+    </>
   );
 }
 
