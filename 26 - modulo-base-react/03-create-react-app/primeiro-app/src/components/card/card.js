215229123c620d02dee698f7062e7812a62554ba
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-16 15:04:08 -0300

    titulo adicionado usando props com função

--- 26 - modulo-base-react/03-create-react-app/primeiro-app/src/components/card/card.js
+++ 26 - modulo-base-react/03-create-react-app/primeiro-app/src/components/card/card.js
@@ -1,15 +1,24 @@
 import React from "react";
 import './card.css'
 
-class Card extends React.Component{
-    render(){
+const Card = ({title}) => {
         return(
             <div className="card">
-                <h3>{this.props.title}</h3>
+                <h3>{title}</h3>
                 <p>Esse é o texto do card</p>
             </div>
         )
-    }
 }
+
+// class Card extends React.Component{
+//     render(){
+//         return(
+//             <div className="card">
+//                 <h3>{this.props.title}</h3>
+//                 <p>Esse é o texto do card</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default Card
