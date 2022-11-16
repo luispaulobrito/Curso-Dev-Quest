51519c688da5ca6d6979486fbcc9707c2b20938b
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-16 14:58:11 -0300

    titulo adicionado usando props

--- 26 - modulo-base-react/03-create-react-app/primeiro-app/src/App.js
+++ 26 - modulo-base-react/03-create-react-app/primeiro-app/src/App.js
@@ -5,9 +5,9 @@ import Card from "./components/card/card.js";
 function App() {
   return (
     <>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card title='Título card 1'/>
+      <Card title='Título card 2'/>
+      <Card title='Título card 3'/>
     </>
   );
 }
--- 26 - modulo-base-react/03-create-react-app/primeiro-app/src/components/card/card.js
+++ 26 - modulo-base-react/03-create-react-app/primeiro-app/src/components/card/card.js
@@ -5,7 +5,7 @@ class Card extends React.Component{
     render(){
         return(
             <div className="card">
-                <h3>Título</h3>
+                <h3>{this.props.title}</h3>
                 <p>Esse é o texto do card</p>
             </div>
         )
