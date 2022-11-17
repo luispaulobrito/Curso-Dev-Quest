97ed32a2542e2a1d5721beb06346c7668a8d0629
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-17 15:06:11 -0300

    componente card aninhado dentro de cards chamado por App.js

--- 26 - modulo-base-react/03-create-react-app/primeiro-app/src/App.js
+++ 26 - modulo-base-react/03-create-react-app/primeiro-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Button from "./components/button/button.js";
-import Card from "./components/card/card.js";
+import Cards from './components/cards/cards.js';
 
 function App() {
   return (
     <>
-      <Button label='Baixar CV'/>
-      <Button/>
+      <Cards/>      
     </>
   );
 }
---
+++ 26 - modulo-base-react/03-create-react-app/primeiro-app/src/components/cards/cards.js
@@ -0,0 +1,26 @@
+import Card from "../card/card";
+
+const Cards = () => {
+  return (
+    <div>
+      <h2>Meus cards</h2>
+
+      <div>
+        <Card>
+          <h3>Título card 1</h3>
+          <p>Esse é o texto do card</p>
+        </Card>
+        <Card>
+          <h3>Título card 2</h3>
+          <p>Esse é o texto do card</p>
+        </Card>
+        <Card>
+          <h3>Título card 3</h3>
+          <p>Esse é o texto do card</p>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
