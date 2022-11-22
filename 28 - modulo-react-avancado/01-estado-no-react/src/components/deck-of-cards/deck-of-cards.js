a3d67cd82baa044ddbfaa09673db7fb97d330e7e
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-22 15:51:56 -0300

    codigo refatorado usando Hook useState

--- 28 - modulo-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
+++ 28 - modulo-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -1,49 +1,56 @@
-import { Component } from "react";
+import { useState } from "react";
 
-async function createDeck(){
-    const response =  await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    const deck = await response.json()
-    return deck.deck_id
+async function createDeck() {
+  const response = await fetch(
+    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  );
+  const deck = await response.json();
+  return deck.deck_id;
 }
 
 async function getCards(deckId) {
-    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
-    return await response.json()   
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+  );
+  return await response.json();
 }
 
-class DeckOfCards extends Component{
-    constructor(){
-        super()
-        this.state = {
-            cards: []
-        }
-    }
-    
-    async componentDidMount(){
-        const deckId = await createDeck()
-        const cards = await getCards(deckId)
-        this.setState({
-            cards: cards.cards
-        })
-    }
+const DeckOfCards = () => {
+  const [deck, setDeck] = useState({
+    cards: [],
+  });
+  return (
+    <section>
+      <ul>
+        {deck.cards.map((card, index) => {
+          <li key={index}>
+            <img src={card.image} alt={card.value} />
+          </li>;
+        })}
+      </ul>
+    </section>
+  );
+};
+//     constructor(){
+//         super()
+//         this.state = {
+//             cards: []
+//         }
+//     }
 
-    render(){
-        return (
-            <section>
-                <ul>
-                    {
-                        this.state.cards.map((card, index)=>{
-                            return(
-                                <li key={index}>
-                                    <img src={card.image} alt={card.value} />
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </section>
-        )
-    }
-}
+//     async componentDidMount(){
+//         const deckId = await createDeck()
+//         const cards = await getCards(deckId)
+//         this.setState({
+//             cards: cards.cards
+//         })
+//     }
 
-export default DeckOfCards
+//     render(){
+//         return (
+
+//         )
+//     }
+// }
+
+export default DeckOfCards;
