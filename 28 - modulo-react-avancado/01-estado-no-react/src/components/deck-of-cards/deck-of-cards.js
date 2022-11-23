b2e7c213fb787acaa154f7d6ca717dfe7851f7bc
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-23 15:54:11 -0300

    renderização condicional e formulários

--- 28 - modulo-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
+++ 28 - modulo-react-avancado/01-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Form from '../../components/forms/form.js'
 
 async function createDeck() {
   const response = await fetch(
@@ -10,15 +11,30 @@ async function createDeck() {
 
 async function getCards(deckId) {
   const response = await fetch(
-    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
   );
   return await response.json();
+}
+
+
+const CardsList = (props) => {
+  return(
+      <ul>
+        {props.cards.map((card, index) => {
+          return (
+            <li key={index}>
+              <img src={card.image} alt={card.value} />
+            </li>
+          );
+        })}
+      </ul>
+  )  
 }
 
 const DeckOfCards = () => {
   const [deck, setDeck] = useState({
     cards: [],
-  });
+  })
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,19 +47,21 @@ const DeckOfCards = () => {
     };
     fetchData();
   },[]);
+
+  const addCard = (newCard) => {
+    console.log(newCard);
+    setDeck({
+      cards: [...deck.cards, newCard] //aqui está criando um array novo com o array antigo mais o novo objeto que será adicionado
+    })
+  }
+  
+
   return (
     <section>
-      <ul>
-        {deck.cards.map((card, index) => {
-          return (
-            <li key={index}>
-              <img src={card.image} alt={card.value} />
-            </li>
-          );
-        })}
-      </ul>
+      <Form addCard={addCard}/>
+      {deck.cards.length > 0 ? <CardsList cards={deck.cards} /> : "Nenhuma carta encontrada"}
     </section>
-  );
-};
+  )
+}
 
 export default DeckOfCards;
